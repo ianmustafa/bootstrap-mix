79bcccac94c02eba03a1c3eacca568800ac10e2f
Ian Mustafa <cuplizian@gmail.com>
2018-12-15 23:18:43 +0700

    Bugfix untuk migrasi Mix ke v4

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
 try {
   window.Popper = require('popper.js').default;
-  window.$ = window.jQuery = require('jquery');
+  window.$ = window.jQuery = require('jquery').default;
 
   // Halaman basic biasanya hanya akan butuh ini
-  require('bootstrap/js/dist/button');
-  require('bootstrap/js/dist/collapse');
-  require('bootstrap/js/dist/dropdown');
-  require('bootstrap/js/dist/modal');
-  require('bootstrap/js/dist/tab');
+  require('bootstrap/js/dist/button').default;
+  require('bootstrap/js/dist/collapse').default;
+  require('bootstrap/js/dist/dropdown').default;
+  require('bootstrap/js/dist/modal').default;
+  require('bootstrap/js/dist/tab').default;
 } catch (e) { }
